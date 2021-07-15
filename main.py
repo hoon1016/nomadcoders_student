5c1aad0f9a0265e5306822b6f3713eef9e8a604a
hoon1016 <80316237+hoon1016@users.noreply.github.com>
2021-07-15 15:30:18 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ from indeed import extract_indeed_pages,extract_indeed_jobs
 
 last_indeed_page = extract_indeed_pages()
 
-extract_indeed_jobs(last_indeed_page)
+indeed_jobs= extract_indeed_jobs(last_indeed_page)
 
+print(indeed_jobs)
 
 
 #순서요약
