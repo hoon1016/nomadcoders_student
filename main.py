f75542ac8c96ef9c70be236e79046da5d1cb0faf
hoon1016 <80316237+hoon1016@users.noreply.github.com>
2021-07-15 13:16:54 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,9 @@
-import requests
-from bs4 import BeautifulSoup
+from indeed import extract_indeed_pages,extract_indeed_jobs
 
-indeed_result=requests.get("https://www.indeed.com/jobs?q=python&limit=50&start=9999")
+last_indeed_page = extract_indeed_pages()
 
-indeed_soup = BeautifulSoup(indeed_result.text,"html.parser")
+extract_indeed_jobs(last_indeed_page)
 
-
-pagination = indeed_soup.find("div", {"class":"pagination"})
-
-links = pagination.find_all('a')
-pages = []
-
-for link in links[1:]:
-    pages.append(int(link.string))
-pages = pages[0:-1]
-
-max_page = pages[-1]+1 
-print(max_page)
 
 
 #순서요약
